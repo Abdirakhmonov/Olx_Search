00ae00d5d8704a6642180cb29a2f6821c40df271
Abdirakhmonov <abdirahmonovakramjon@mail.ru>
2024-05-30 19:40:12 +0500

    first commit

---
+++ lib/pages/spiska_page.dart
@@ -0,0 +1,35 @@
+
+import 'package:flutter/material.dart';
+import '../data/list_data.dart';
+import '../widgets/cart_widget.dart';
+
+
+class SipiskaPage extends StatelessWidget {
+  const SipiskaPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Data dataList = Data();
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: dataList.data.length,
+              itemBuilder: (context, index) {
+                final item = dataList.data[index];
+                return SpiskaCardWidget(
+                  image: item["images"],
+                  text: item["text"],
+                  price: item["price"],
+                  location: item["location"],
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
